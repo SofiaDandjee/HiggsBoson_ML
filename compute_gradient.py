f601562a55e1b78988ce8631f72df51b9dfd46ea
Héloïse Monnet <heloise.monnet@epfl.ch>
2019-10-20 12:45:29 +0200

    add cross validation

--- compute_gradient.py
+++ compute_gradient.py
@@ -2,11 +2,9 @@ import numpy as np
 
 def compute_gradient(y, tx, w):
     """Compute the gradient."""
-
     num_samples = len(y)
     error = y-np.dot(tx,w)
     grad_w = (-1/num_samples)*np.dot(tx.transpose(),error)
-
     return error, grad_w
 
 def calculate_logistic_gradient(y, tx, w):
