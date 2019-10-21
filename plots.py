32e865e8904d0b1c12dbaeec4feb0dede9632068
Marie Anselmet <MarieAnselmet@tsf-428-wpa-6-244.epfl.ch>
2019-10-21 13:58:40 +0200

    bkabla

--- plots.py
+++ plots.py
@@ -2,12 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_cross_validation(extra_param, best_extra_param, rmse_train, rmse_test, loss):
+def plot_cross_validation(extra_param, extra_param_name, best_extra_param, rmse_train, rmse_test, loss):
     """Visualization of the curves of rmse_train and rmse_test depending on the extra parameter (lambda or gamma)."""
     plt.plot(extra_param, rmse_train, color='b', label="Train error")
     plt.plot(extra_param, rmse_test, color='g', label="Test error")
     plt.plot(best_extra_param, loss, 'r*', markersize=15, label="Minimal loss")
-    plt.xlabel("Lambda")
+    plt.xlabel(extra_param_name)
     plt.ylabel("RMSE")
     plt.title("Cross validation")
     plt.legend(loc=2)
