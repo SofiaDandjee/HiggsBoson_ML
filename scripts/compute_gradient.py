f0151fbb69bdb5d16611822d21774ba0fc54cb26
Sofia Dandjee <sofia.dandjee@epfl.ch>
2019-10-27 13:10:37 +0100

    scripts folder

---
+++ scripts/compute_gradient.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def sigmoid(t):
+    """Sigmoid function."""
+    return 1/(1+np.exp(-t))
+
+def compute_gradient(y, tx, w):
+    """Compute the gradient for the linear regression."""
+    num_samples = len(y)
+    error = y-np.dot(tx,w)
+    grad_w = (-1/num_samples)*np.dot(tx.transpose(),error)
+    return error, grad_w
+
+def logistic_gradient(y, tx, w):
+    """Compute the gradient of logistic loss."""
+    gradient = np.dot(tx.transpose(),(sigmoid(np.dot(tx,w))-y))
+    return gradient
+
+def reg_logistic_gradient(y, tx, w, lambda_):
+    """Compute the gradient of regularized logistic loss."""
+    gradient = logistic_gradient(y, tx, w) + 2 * lambda_ * w
+    return gradient
---
+++ scripts/proj1_helpers.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""some helper functions for project 1."""
+import csv
+import numpy as np
+
+
+def load_csv_data(data_path, sub_sample=False):
+    """Loads data and returns y (class labels), tX (features) and ids (event ids)"""
+    y = np.genfromtxt(data_path, delimiter=",", skip_header=1, dtype=str, usecols=1)
+    x = np.genfromtxt(data_path, delimiter=",", skip_header=1)
+    ids = x[:, 0].astype(np.int)
+    input_data = x[:, 2:]
+
+    # convert class labels from strings to binary (-1,1)
+    yb = np.ones(len(y))
+    yb[np.where(y=='b')] = -1
+    
+    # sub-sample
+    if sub_sample:
+        yb = yb[::50]
+        input_data = input_data[::50]
+        ids = ids[::50]
+
+    return yb, input_data, ids
+
+def compute_f1_score(y, tx, w, regression = 'linear'):
+    """Compute the f1 score, ie the harmonic mean of p and r where p is the number
+    of correct positive results divided by the number of all positive results returned by our prediction,
+    and r is the number of correct positive results divided by the number of all relevant samples."""
+    y_pred_training = predict_labels(w, tx, regression)
+    num_samples = len(y_pred_training)
+    ind_pos_pred = np.ravel(np.where(y_pred_training==1))
+    #Number of positive results returned by our prediction
+    count_pos_training = ind_pos_pred.shape[0]
+    #Number of relevant samples (ie positive results in y)
+    count_relevant_pos = 0
+    #Number of correct positive results
+    count_true_pos = 0
+                           
+    if regression == 'linear':
+        for i in range(num_samples):
+             if (y[i] == 1):
+                count_relevant_pos += 1
+                if (y_pred_training[i] == 1):
+                    count_true_pos += 1
+    
+                           
+    elif regression == 'logistic': 
+        for i in range(num_samples):
+            if (y[i] == 1):
+                count_relevant_pos += 1
+                if (y_pred_training[i] == 1):
+                    count_true_pos += 1         
+    
+    p = count_true_pos/count_pos_training
+    r = count_true_pos/count_relevant_pos
+    # Harmonic mean
+    f1_score = 2/(1/p + 1/r)
+    print(f1_score)
+    return f1_score
+
+def predict_accuracy(y, tx, w, regression = 'linear'):
+    """Compute the percentage of matching between predictions and test data."""
+    y_pred_training = predict_labels(w, tx, regression)
+    num_samples = len(y_pred_training)
+    count = 0
+    if regression == 'linear':
+        for i in range(num_samples):
+            if (y_pred_training[i] == y[i]):
+                count += 1
+    elif regression == 'logistic':
+        for i in range(num_samples):
+            if (y_pred_training[i] == -1 and y[i] == 0):
+                count += 1
+            if (y_pred_training[i] == 1 and y[i] == 1):
+                count +=1
+                
+    accuracy = (count *100) / num_samples
+    return accuracy
+
+def predict_labels(weights, data, regression = 'linear'):
+    """Generates class predictions given weights, and a test data matrix"""
+    y_pred = np.dot(data, weights)
+    if regression == 'linear':
+        y_pred[np.where(y_pred <= 0)] = -1
+        y_pred[np.where(y_pred > 0)] = 1
+    elif regression == 'logistic':
+        y_pred[np.where(y_pred <= 0.5)] = -1
+        y_pred[np.where(y_pred > 0.5)] = 1
+        
+    return y_pred
+
+
+def create_csv_submission(ids, y_pred, name):
+    """
+    Creates an output file in csv format for submission to kaggle
+    Arguments: ids (event ids associated with each prediction)
+               y_pred (predicted class labels)
+               name (string name of .csv output file to be created)
+    """
+    with open(name, 'w') as csvfile:
+        fieldnames = ['Id', 'Prediction']
+        writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=fieldnames)
+        writer.writeheader()
+        for r1, r2 in zip(ids, y_pred):
+            writer.writerow({'Id':int(r1),'Prediction':int(r2)})
